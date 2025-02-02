7bb4a6fa74266a72a643d4908a229deedc982226
rakesh baleri <rrbaleri@gmail.com>
2025-02-02 08:21:36 +0530

    change 2

--- jenkins.java
+++ jenkins.java
@@ -3,6 +3,10 @@ import java.util.Date;
 class jenkins {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Jenkins i have made some changes");
+        System.out.println(new Date());
+        System.out.println("Hello World");
+        System.out.println("Hello Jenkins i have made some changes");
         System.out.println(new Date());
     }
 }
